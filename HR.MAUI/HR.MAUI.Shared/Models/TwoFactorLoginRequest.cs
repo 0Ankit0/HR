8474dd6d336b95fcaee7e15a86facd63531a6a5f
0ankit0 <ankitpaudel.1373@gmail.com>
2025-08-09 07:23:20 +0545

    feat: moved all the files to maui project and removed previous blazor server project

---
+++ HR.MAUI/HR.MAUI.Shared/Models/TwoFactorLoginRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Shared.Models
+{
+    public class TwoFactorLoginRequest
+    {
+
+        public string TwoFactorCode { get; set; } = string.Empty;
+        public bool RememberMe { get; set; }
+        public bool RememberMachine { get; set; }
+        public string? ReturnUrl { get; set; }
+    }
+}
--- HR.MAUI/HR.MAUI.Web/Program.cs
+++ HR.MAUI/HR.MAUI.Web/Program.cs
@@ -1,12 +1,37 @@
-using HR.MAUI.Web.Components;
+using HR.Config;
 using HR.MAUI.Shared.Services;
+using HR.MAUI.Web.Components;
 using HR.MAUI.Web.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+builder.Services.AddIdentityAndDb(builder.Configuration);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddTransient<AuthenticationHandler>();
+
+builder.Services.AddHttpClient("Default", httpClient =>
+{
+    httpClient.BaseAddress = new Uri(builder.Configuration["BaseUrl"] ?? "https://localhost:7073/");
+})
+.AddHttpMessageHandler<AuthenticationHandler>();
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("Default"));
+
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+builder.Services.AddEndpoints();
 
 // Add device-specific services used by the HR.MAUI.Shared project
 builder.Services.AddSingleton<IFormFactor, FormFactor>();
@@ -23,11 +48,16 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddAdditionalAssemblies(typeof(HR.MAUI.Shared._Imports).Assembly);
+
+app.MapEndpoints();
 
 app.Run();
